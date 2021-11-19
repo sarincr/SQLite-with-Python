0486c02ccff54a949c484dfc7123211772a5d02d
sarincr <sarinrays@gmail.com>
2021-11-19 08:46:44 +0530

    Search and add

--- 07.Multi/NewDB.py
+++ 07.Multi/NewDB.py
@@ -10,12 +10,11 @@ cursor = conn.cursor()
 table ="""CREATE TABLE STUDENT(NAME VARCHAR(255), CLASS VARCHAR(255), SECTION VARCHAR(255));"""
 cursor.execute(table)
 
-students = [('John', '8', 'A'),
-   ('Adam', '8', 'B'),
-   ('Ann', '10', 'A'),
-]
 
-cursor.executemany('INSERT INTO STUDENT VALUES (?,?,?)', students)
+cursor.execute('''INSERT INTO STUDENT VALUES ('John', '8', 'A')''')
+cursor.execute('''INSERT INTO STUDENT VALUES ('Adam', '8', 'B')''')
+cursor.execute('''INSERT INTO STUDENT VALUES ('Ann', '10', 'A')''')
+
 
 print("Data Inserted in the table: ")
 data=cursor.execute('''SELECT * FROM STUDENT''')
